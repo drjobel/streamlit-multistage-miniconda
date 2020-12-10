21b65f887c99231bb54775ab8431843fdcd1d007
Jose Beltran <drjobel.connection@gmail.com>
2020-12-10 12:43:43 +0100

    Added example of RESTRICTED services implementation

--- project/services/etl.py
+++ project/services/etl.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title('Extract-Transform-Load (ETL)')
+# Extract-Transform-Load (ETL)
+
+with st.beta_expander(label="Input files", expanded=True):
+   
+    uploaded_file =  st.file_uploader('', )
+
+    st.write("""
+    Only `tab` separated text files with `encoding='utf-8` are accepted.
+    
+    ***NOTE:*** *`max uploaded size = 200 MB`)*.""")
+    
+    if uploaded_file is not None:
+        st.write(uploaded_file )
