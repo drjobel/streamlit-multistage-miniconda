2a720a2b4c340deaf8464e707f2b9f04560989d3
Jose Beltran <drjobel.connection@gmail.com>
2020-12-10 13:04:02 +0100

    Minor changes.Typos

--- project/app.py
+++ project/app.py
@@ -1,14 +1,11 @@
 import os
-import sys
 import streamlit as st
-import turpy
 from services import get_available_activities
 from config import SERVICES_YAML_URL
 
 
 def main():
-    st.sidebar.title('Menu:')
-    st.title(f'AquaBiota Water Research')
+    st.sidebar.title('AquaBiota')  
 
     selected_service, _ = get_available_activities(
     filepath=os.path.abspath(SERVICES_YAML_URL), label="services:", key='services')
