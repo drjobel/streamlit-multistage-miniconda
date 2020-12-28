fa47ea9b69fbf599608d88e550c83794bbf5c1ee
Jose Beltran <drjobel.connection@gmail.com>
2020-12-28 16:09:28 +0100

    WIP

--- project/config.py
+++ project/config.py
@@ -1,3 +1,4 @@
 SERVICES_YAML_URL = './project/app_services.yaml'
 SHELVE_FILEPATH = './data/aquabiota_shelve'
 
+FILE_TYPES = ["yaml", "yml"]
---
+++ project/io.py
@@ -0,0 +1,7 @@
+import os
+from pandas import read_csv, DataFrame
+from typing import Callable, BytesIO
+
+from turpy.logger import log
+from turpy.io.load_yaml import load_yaml
+
